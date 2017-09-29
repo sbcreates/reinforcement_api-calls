85b717c3011b88b6a063d442dc01c8533a6f7754
sbcreates <drawsarahb@gmail.com>
2017-09-29 16:48:49 -0400

    Added httparty. Rendered json data on page of representatives

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,16 @@
-class RepresentativesController < ApplicationController
-  def index
-  end
+require 'httparty'
 
-  def show
+class RepresentativesController < ApplicationController
+
+  def index
+    response = HTTParty.get('https://represent.opennorth.ca/representatives/')
+    parsed_response = JSON.parse(response.body)
+
+    useful_data = parsed_response['objects']
+
+    render json:
+      useful_data.map { |data| {first_name: data["first_name"], last_name: data["last_name"], district: data["district_name"], elected_office: data["elected_office"], email: data["email"]}
+      }
+
   end
 end
